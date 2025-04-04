3805853828cc01e99b89f85e61d0b6a4dfef53e2
nordinMunteanu <149517750+nordinMunteanu@users.noreply.github.com>
2025-04-04 12:33:51 +0300

    Update NordComp.bat

--- Compiler/NordComp.bat
+++ Compiler/NordComp.bat
@@ -3,20 +3,26 @@
 color 7
 
 g++ -o done scripts\say_done.cpp
-g++ -o temp scripts\source.cpp
+g++ -o precompile scripts\precompiler.cpp
+g++ -o compile scripts\compiler.cpp
 
 set /p filename=Enter the name of the file you want to compile:
 echo Openning file: "%filename%"
 done
 
 echo Precompiling...
-temp %filename%
+precompile %filename%
 done
 
-echo Sending to the compiler...
+echo Compiling...
+compile
+done
+
+echo Sending to the programmer...
 stc.ttl
 done
 
 del done.exe
-del temp.exe
+del precompile.exe
+del compile.exe
 cmd /k
