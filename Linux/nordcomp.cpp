16b66bb3ac71415c8fceae3f37436c8b507b252f
nordinMunteanu <149517750+nordinMunteanu@users.noreply.github.com>
2025-04-14 22:33:14 +0300

    Update nordcomp.cpp

--- Linux/nordcomp.cpp
+++ Linux/nordcomp.cpp
@@ -3,21 +3,49 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <map>
+
+std::map<std::string, int> v = {
+    {"c", 0},
+    {"help", 1},
+    {"k-fab", 2},
+    {"k-hex", 3},
+    {"k-comp", 4},
+    {"k-precomp", 5},
+    {"k-upl", 6}
+};
 
 int main(int argc, char* argv[]){
-    if(std::strcmp(argv[1], "-c") == 0){
-        auto joined_args = [&]() -> std::string{
-            std::string result;
-            for(int i = 0; i < argc; ++i){
-                if(i!=0) result += " ";
-                result += argv[i];
-            }
-            return result;
-        }();
-        std::printf("the arguments are:%s", joined_args.c_str());
-    } else if(argv[1] == "help"){
+    int comv[7] = {0};
 
-    } else std::cout<<"Invalid argument\n";
+    if(argc < 1){
+        std::cout<<"Not enough arguments. Use --help for a list of commands";
+    }
+    else{
+        for(int i = 1; i <= argc; ++i){
+            std::string com(argv[i]);
+            if(com[0] == '-'){
+                if(com[1] == '-'){
+                    com.erase(com.begin()+1);
+                    comv[v[com]]++;
+                }
+                else {
+                    com.erase(com.begin());
+                    while(!com.empty()){
+                        std::string com2(1, com[0]);
+                        comv[v[com2]]++;
+                        com.erase(com.begin());
+                    }
+                }
+            }
+            else std::cout<<"Wrong argument format\n";
+        }
+    }
+
+    for(int i = 0; i < 7; ++i){
+        std::cout<<comv[i]<<' ';
+    }
+    std::cout<<'\n';
 
     return 0;
 }
