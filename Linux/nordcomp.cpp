0a664fc333d16f0bc720139d004be93e9b908ee7
nordinMunteanu <149517750+nordinMunteanu@users.noreply.github.com>
2025-04-15 08:44:28 +0300

    Update nordcomp.cpp

--- Linux/nordcomp.cpp
+++ Linux/nordcomp.cpp
@@ -15,6 +15,17 @@ std::map<std::string, int> v = {
     {"k-upl", 6}
 };
 
+std::map<int, std::string> vrev = {
+    {0, "c"},
+    {1, "help"},
+    {2, "k-fab"},
+    {3, "k-hex"},
+    {4, "k-comp"},
+    {5, "k-precomp"},
+    {6, "k-upl"}
+};
+
+
 int main(int argc, char* argv[]){
     int comv[7] = {0};
 
@@ -22,30 +33,56 @@ int main(int argc, char* argv[]){
         std::cout<<"Not enough arguments. Use --help for a list of commands";
     }
     else{
-        for(int i = 1; i <= argc; ++i){
+        for(int i = 1; i < argc; ++i){
             std::string com(argv[i]);
+
             if(com[0] == '-'){
-                if(com[1] == '-'){
-                    com.erase(com.begin()+1);
+                if(com[1] == '-') {
+                    com.erase(com.begin());
+                    com.erase(com.begin());
+                    if(v[com] == 0){
+                        std::cout<<"Unknown argument\n";
+                        return 2;
+                    }
                     comv[v[com]]++;
                 }
-                else {
+                else{
                     com.erase(com.begin());
-                    while(!com.empty()){
-                        std::string com2(1, com[0]);
-                        comv[v[com2]]++;
-                        com.erase(com.begin());
+                    for(int j = 0; j < com.size(); ++j){
+                        std::string tmp(1, com[j]);
+                        if(v[tmp] == 0 && tmp != "c"){
+                            std::cout<<"Unknown argument\n";
+                            return 2;
+                        }
+                        comv[v[tmp]]++;
                     }
                 }
             }
-            else std::cout<<"Wrong argument format\n";
+            else{
+                 std::cout<<"Invalid argument format\n";
+                 return 1;
+            }
         }
     }
 
+    std::string instr("./runner.sh ");
+
     for(int i = 0; i < 7; ++i){
-        std::cout<<comv[i]<<' ';
+        if(comv[i] > 1){
+            std::cout<<"Invalid argument repetition\n";
+            return 3;
+        }
+
+        if(comv[i] != 0){
+            instr+=vrev[i];
+            instr+=" ";
+            comv[i]--;
+        }else{
+            instr+="0 ";
+        }
     }
-    std::cout<<'\n';
+
+    std::cout<<instr<<'\n';
 
     return 0;
 }
